299b6b55fa65930a752d6f43a180d21a4189e94e
koala0521 <328762691@qq.com>
2018-05-02 20:00:40 +0800

    初始化项目

---
+++ src/componets/header/index.vue
@@ -0,0 +1,64 @@
+<style scoped >
+    .ivu-layout-header{
+        background: #fff;
+    }
+    .layout-logo{
+        width: 100px;
+        height: 30px;
+        border-radius: 3px;
+        float: left;
+        position: relative;
+        top: 15px;
+        left: 20px;
+        margin-right: 100px;
+    }
+    .layout-logo img{
+        width: 100%;
+        height: 100%;
+    }
+    .ivu-menu{
+        border:none;
+    }
+
+</style>
+
+<template>
+    <Header :style="{position: 'fixed', width: '100%'}">
+        <Menu mode="horizontal" theme="light" active-name="1">
+            <div class="layout-logo">
+                <img src="../../assets/img/logo.png" alt="">
+            </div>
+            <div class="layout-nav">
+                <MenuItem name="1">
+                    <Icon type="ios-navigate"></Icon>
+                    Item 1
+                </MenuItem>
+                <MenuItem name="2">
+                    <Icon type="ios-keypad"></Icon>
+                    Item 2
+                </MenuItem>
+                <MenuItem name="3">
+                    <Icon type="ios-analytics"></Icon>
+                    Item 3
+                </MenuItem>
+                <Submenu name="3">
+                    <template>
+                        <Icon type="stats-bars"></Icon>
+                        统计分析
+                    </template>
+                    <MenuItem name="3-4">用户留存</MenuItem>
+                    <MenuItem name="3-5">流失用户</MenuItem>
+
+                </Submenu>
+            </div>
+            <div>
+                
+            </div>
+        </Menu>
+    </Header>
+</template>
+<script>
+    export default{
+
+    }
+</script>
---
+++ src/views/index.vue
@@ -0,0 +1,40 @@
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #fff;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+
+    .layout-nav{
+        width: 420px;
+        margin: 0 auto;
+        margin-right: 20px;
+    }
+    .layout-footer-center{
+        text-align: center;
+    }
+    .ivu-layout{
+        background: #fff;
+    }
+</style>
+<template>
+    <div class="layout">
+        <Layout>
+            <wHeader></wHeader>
+            <Content :style="{margin: '88px 20px 0', minHeight: '500px'}">
+                Content
+            </Content>
+            <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+        </Layout>
+    </div>
+</template>
+<script>
+    import wHeader from '../componets/header/index';
+    export default {
+        components:{
+            wHeader
+        }
+    };
+</script>
